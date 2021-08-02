dde5ddfb4eb1dd89849c0e6eec38619420cf1d60
Jaxtyn <kny323@smarthaus.co.kr>
2021-08-02 16:55:26 +0900

    Implement restaurant example graphql
    - Add nestConfiguration
    - Add typeorm

---
+++ src/restaurants/restaurants.resolvers.ts
@@ -0,0 +1,18 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { RestaurantService } from './restaurants.service';
+
+@Resolver(() => Restaurant)
+export class RestaurantResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
+
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+  @Mutation(() => Boolean)
+  createRestaurant(@Args() createResturantInput: CreateRestaurantDto): boolean {
+    return true;
+  }
+}
